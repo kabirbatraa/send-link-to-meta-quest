387b7242528e12212428b5ea52cf39ed0edb2def
Kabir Batra <kabirbatraa@gmail.com>
2024-12-29 20:09:40 -0800

    add https if missing

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div style={{height: "100vh", padding:"20%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
           {/* <div style={{width: "50%"}}> */}
-            <Input type="url" placeholder="https://google.com" pattern="https://.*" required 
+            <Input type="url" placeholder="https://google.com"
               style={{marginBottom: "10px"}}
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
@@ -23,9 +23,23 @@ function App() {
 
               let url = inputValue;
 
-              if (inputValue == "") {
+              if (url == "") {
                 url = "https://google.com"
               }
+
+              // Add https:// if missing
+              else if (!url.startsWith('http://') && !url.startsWith('https://')) {
+                url = 'https://' + url;
+              }
+              
+              // validation is useless if we add https:// when it is missing
+              // // validate url using URL constructor
+              // try {
+              //   new URL(url);
+              //   window.open('https://www.oculus.com/open_url/?url=' + url, '_blank', 'noopener,noreferrer');
+              // } catch (e) {
+              //   alert("Please enter a valid URL");
+              // }
 
               window.open('https://www.oculus.com/open_url/?url=' + url, '_blank', 'noopener,noreferrer')
             }} >Send</Button>
