e79b85766d4e6195db3f8bb955db81ca290fb6ee
Kabir Batra <kabirbatraa@gmail.com>
2024-12-29 23:21:24 -0800

    setup for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/send-link-to-meta-quest',
   plugins: [react()],
   resolve: {
     alias: {
