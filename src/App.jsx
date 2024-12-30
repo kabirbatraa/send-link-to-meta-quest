012b46a42a5976d440ff09dd14a603a25df395ec
Kabir Batra <kabirbatraa@gmail.com>
2024-12-29 20:15:20 -0800

    add a title

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,39 @@ function App() {
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div style={{height: "100vh", padding:"20%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-          {/* <div style={{width: "50%"}}> */}
-            <Input type="url" placeholder="https://google.com"
-              style={{marginBottom: "10px"}}
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <Button onClick={() => {
-              // console.log("hello");
+          <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            Send link to Meta Quest
+          </h2>
+          <Input type="url" placeholder="https://google.com"
+            style={{marginBottom: "10px"}}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button onClick={() => {
+            // console.log("hello");
 
-              let url = inputValue;
+            let url = inputValue;
 
-              if (url == "") {
-                url = "https://google.com"
-              }
+            if (url == "") {
+              url = "https://google.com"
+            }
 
-              // Add https:// if missing
-              else if (!url.startsWith('http://') && !url.startsWith('https://')) {
-                url = 'https://' + url;
-              }
-              
-              // validation is useless if we add https:// when it is missing
-              // // validate url using URL constructor
-              // try {
-              //   new URL(url);
-              //   window.open('https://www.oculus.com/open_url/?url=' + url, '_blank', 'noopener,noreferrer');
-              // } catch (e) {
-              //   alert("Please enter a valid URL");
-              // }
+            // Add https:// if missing
+            else if (!url.startsWith('http://') && !url.startsWith('https://')) {
+              url = 'https://' + url;
+            }
+            
+            // validation is useless if we add https:// when it is missing
+            // // validate url using URL constructor
+            // try {
+            //   new URL(url);
+            //   window.open('https://www.oculus.com/open_url/?url=' + url, '_blank', 'noopener,noreferrer');
+            // } catch (e) {
+            //   alert("Please enter a valid URL");
+            // }
 
-              window.open('https://www.oculus.com/open_url/?url=' + url, '_blank', 'noopener,noreferrer')
-            }} >Send</Button>
-          {/* </div> */}
+            window.open('https://www.oculus.com/open_url/?url=' + url, '_blank', 'noopener,noreferrer')
+          }} >Send</Button>
         </div>
       </ThemeProvider>
     </>
